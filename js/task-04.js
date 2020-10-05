e39d52bdcb6fc439a91b7137dd982a386919df8d
Капинус Юлия <kapinusjuli55@gmail.com>
2020-10-05 08:12:10 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,25 @@
 
-const actions = document.querySelectorAll('button');
+const actions = Array.from(document.querySelectorAll('button'));
 console.log(actions);
 console.log(actions[0].dataset.action);
 
+const numbers = actions;
 
-const targetBtn = document.querySelector('.js-target-btn');
-const addListenerBtn = document.querySelector('.js-add-listener');
-const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+numbers.forEach(num => console.log(num.dataset.action));
+
+const summ = numbers.reduce((acc, value) => acc + value, 0);
+
+console.log(summ); // 15
+
+
+
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+let counterValue = 0;
+
 
 addListenerBtn.addEventListener('click', event => {
   console.log(event);
@@ -15,14 +28,14 @@ addListenerBtn.addEventListener('click', event => {
   targetBtn.addEventListener('click', onTargetBtnClick);
 });
 
-removeListenerBtn.addEventListener('click', event => {
-  console.log(event);
-  console.log('Снимаю слушателя события с целевой кнопки');
+// removeListenerBtn.addEventListener('click', event => {
+//   console.log(event);
+//   console.log('Снимаю слушателя события с целевой кнопки');
 
-  targetBtn.removeEventListener('click', onTargetBtnClick);
-});
+//   targetBtn.removeEventListener('click', onTargetBtnClick);
+// });
 
-function onTargetBtnClick(event) {
-  console.log(event);
-  console.log('Клик по целевой кнопке');
-}
+// function onTargetBtnClick(event) {
+//   console.log(event);
+//   console.log('Клик по целевой кнопке');
+// }
