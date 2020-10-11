8387a5b3906cb715e74e868c5099602d3bb8312c
Капинус Юлия <kapinusjuli55@gmail.com>
2020-10-11 17:38:53 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -26,5 +26,33 @@ function onInputChange(event) {
   textEl.style.fontSize = `${event}px`;
 
 }
+// Не понимаю, правильно ли поняла, но дебаггер ниже нашла по ссылке:
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/debugger
+
+function potentiallyBuggyCode() {
+  debugger;
+  
+  // чтобы протестировать работу отладчика, поместите здесь код, заведомо содержащий ошибку
+
+  const inputEl = document.querySelector('input');
+  
+  console.log(inputEl.id);
+  
+  inputEl.innerHTML = '  min="10" max="30" step="1"';
+  
+  console.log(inputEl);
+  
+  const textEl = document.querySelector('#text');
+  console.log(textEl);
+  
+  inputEl.addEventListener('input', onInputChange);
+  
+  function onInputChange(event) {
+    console.log(event);
+    textEl.style.fontSize = `${event}px`;
+  
+  }
+
+}
 
 
