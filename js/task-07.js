b5820e302cdc8b626437df7e089e0bb317bde6b4
Капинус Юлия <kapinusjuli55@gmail.com>
2020-10-11 21:15:07 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -12,9 +12,11 @@ const inputEl = document.querySelector('input');
   
 console.log(inputEl.id);
 
-inputEl.innerHTML = '  min="10" max="30" step="1"';
+inputEl.innerHTML = '  min="10" max="30" value="20" step="1"';
 
 console.log(inputEl);
+
+console.log(inputEl.value);
 
 const textEl = document.querySelector('#text');
 console.log(textEl);
@@ -22,37 +24,25 @@ console.log(textEl);
 inputEl.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  console.log(event);
-  textEl.style.fontSize = `${event}px`;
+    textEl.style.fontSize = `${inputEl.value}px`;
 
 }
-// Не понимаю, правильно ли поняла, но дебаггер ниже нашла по ссылке:
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/debugger
 
-function potentiallyBuggyCode() {
-  debugger;
-  
-  // чтобы протестировать работу отладчика, поместите здесь код, заведомо содержащий ошибку
+// // Не понимаю, правильно ли поняла, но дебаггер ниже нашла по ссылке:
+// // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/debugger
 
-  const inputEl = document.querySelector('input');
+// function potentiallyBuggyCode() {
+//   debugger;
   
-  console.log(inputEl.id);
-  
-  inputEl.innerHTML = '  min="10" max="30" step="1"';
-  
-  console.log(inputEl);
-  
-  const textEl = document.querySelector('#text');
-  console.log(textEl);
-  
-  inputEl.addEventListener('input', onInputChange);
-  
-  function onInputChange(event) {
-    console.log(event);
-    textEl.style.fontSize = `${event}px`;
-  
-  }
+//   // чтобы протестировать работу отладчика, поместите здесь код, заведомо содержащий ошибку
 
-}
+  
+// function onInputChange(event) {
+//   console.log(event);
+//   textEl.style.fontSize = `${event}px`;
+
+// }
+
+// }
 
 
